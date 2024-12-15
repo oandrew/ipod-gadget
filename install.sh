c5b852a40a2982919c9404518b6bfbb3963b9d12
DangerzAU <jimmyc.iphone@gmail.com>
2024-12-15 23:40:55 +1100

    new file:   gadget/dkms.conf Created dkms file to enable simpler installation of kernel module
    	new file:   install.sh
    Created install.sh to configure pi, install dkms and compile/install kernel modules without user interaction.

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+echo "Updating APT sources and updating currently installed software to latest version...."
+echo ""
+# Lets get the pi's software up to date and upgraded before we start
+sudo apt update && sudo apt full-upgrade -y
+# Install Pi Linux Headers if they're not allready install
+echo "Installing kernel header sources for compilation..."
+echo ""
+sudo apt-get install raspberrypi-kernel-headers -y
+# Now we install build dependancies so we can get the modules compiled
+echo "Installing compilation dependancies..."
+echo ""
+sudo apt install git bc bison flex libssl-dev make -y
+# Next we install DKMS without any recommended modules as some DKMS packages have a habit of install the wrong linux kernel headers as well
+echo "Installing DKMS to allow for simpler kernel module install/upgrading..."
+echo ""
+sudo apt install --no-install-recommends dkms
+# Next we clone/pull down the git repo we want to compile
+#echo "Cloning repo..."
+#echo ""
+#git clone https://github.com/dangerzau/ipod-gadget.git
+# Switch into the downloaded directory and setup a softlink for DKMS
+echo "Setting up DKMS local repo..."
+echo ""
+sudo ln -s $PWD/gadget/ /usr/src/ipod-gadget-0.1
+echo "Installing ipod-gadget into DKMS tree..."
+echo ""
+sudo dkms add -m ipod-gadget/0.1
+echo "Compiling and installing module via DKMS..."
+echo ""
+sudo dkms install ipod-gadget/0.1
